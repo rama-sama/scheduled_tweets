501fb7e791c1b3e82bfcbeca6c3a96a0d607070e
rama_sama <matthewramadan@gmail.com>
2021-03-06 15:41:34 -0800

    Add user session functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "sign_up", to: "users#new"
   post "sign_up", to: "users#create"
 
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
